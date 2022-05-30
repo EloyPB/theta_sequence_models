d3d8b2ded5db7adfe635f83833f833324c453e53
Eloy Parra Barrero <eloy.parrabarrero@ruhr-uni-bochum.de>
2022-05-30 19:14:35 +0200

    theta inhibition

--- Network.py
+++ Network.py
@@ -8,8 +8,8 @@ from generic.smart_class import Config, SmartClass
 class Network(SmartClass):
     dependencies = [LinearTrack]
 
-    def __init__(self, num_units, tau, w_rec_sigma, w_rec_exc, w_rec_inh, w_rec_shift=1, sigmoid_gain=1,
-                 sigmoid_midpoint=0.5, config=Config(), d={}):
+    def __init__(self, num_units, tau, w_rec_sigma, w_rec_exc, w_rec_inh, w_rec_shift, sigmoid_gain, sigmoid_midpoint,
+                 theta_min, theta_max, theta_concentration, config=Config(), d={}):
 
         SmartClass.__init__(self, config, d)
 
@@ -31,7 +31,14 @@ class Network(SmartClass):
 
         self.sigmoid_gain = sigmoid_gain
         self.sigmoid_midpoint = sigmoid_midpoint
+        self.theta_max = theta_max
+        self.theta_amplitude = theta_max - theta_min
+        self.theta_concentration = theta_concentration
+        self.theta_concentration_exp = np.exp(theta_concentration)
+        self.theta_cycle_steps = 1 / (8 * self.track.dt)
+
         self.act_log = np.empty((len(self.track.x_log), num_units))
+        self.theta_log = np.empty(len(self.track.x_log))
 
     def plot_rec_weights(self):
         fig, ax = plt.subplots(1, constrained_layout=True)
@@ -57,25 +64,44 @@ class Network(SmartClass):
             for index in range(lap_start_index, last_lap_index):
                 act_out = self.f_act(act)
                 rec_input = self.w_rec @ act_out
-                act += (-act + rec_input) * self.dt_over_tau
+                self.theta_log[index] = self.theta(index)
+                act += (-act + rec_input + self.theta_log[index]) * self.dt_over_tau
                 self.act_log[index] = act.copy()
+
+    def theta(self, index):
+        theta_phase = 2 * np.pi * (index % self.theta_cycle_steps) / self.theta_cycle_steps
+        return (-np.exp(self.theta_concentration * np.cos(theta_phase)) / self.theta_concentration_exp
+                * self.theta_amplitude + self.theta_max)
 
     def f_act(self, x):
         return 1 / (1 + np.exp(-self.sigmoid_gain * (x - self.sigmoid_midpoint)))
 
-    def plot_activities(self, t_start=0, t_end=None):
+    def plot_activities(self, t_start=0, t_end=None, apply_f=False):
         index_start = int(t_start / self.track.dt)
         index_end = int(t_end / self.track.dt) if t_end is not None else len(self.act_log)
 
         act_log = np.array(self.act_log[index_start:index_end])
+        if apply_f:
+            act_log = self.f_act(act_log)
         extent = (index_start * self.track.dt - self.track.dt / 2, index_end * self.track.dt + self.track.dt / 2,
                   -0.5, act_log.shape[1] - 0.5)
 
-        fig, ax = plt.subplots()
+        fig, axes = plt.subplots(2, 2, sharex='col', gridspec_kw={'height_ratios': (1, 0.2), 'width_ratios': (1, 0.03)})
+        ax = axes[0, 0]
         mat = ax.matshow(act_log.T, aspect="auto", origin="lower", extent=extent)
         ax.xaxis.set_ticks_position('bottom')
         ax.set_xlim([extent[0], extent[1]])
         ax.set_title("Network activities")
         ax.set_ylabel("Unit #")
-        color_bar = plt.colorbar(mat, ax=ax)
+        color_bar = plt.colorbar(mat, cax=axes[0, 1])
         color_bar.set_label("Activation")
+
+        ax = axes[1, 0]
+        time = np.arange(len(self.act_log)) * self.track.dt
+        ax.plot(time, self.theta_log)
+        ax.set_ylabel("Theta")
+        ax.set_xlabel("Time (s)")
+
+        axes[1, 1].set_visible(False)
+
+        fig.tight_layout()
--- test_network.py
+++ test_network.py
@@ -2,8 +2,12 @@ from Network import Network, Config, plt
 
 config = Config(variants={'LinearTrack': 'OneLap'})
 network = Network.current_instance(config)
+
 # network.track.plot_trajectory()
+
 # network.plot_rec_weights()
+
 network.run(reset_indices=(0, 10), reset_value=1)
-network.plot_activities()
+network.plot_activities(apply_f=False)
+
 plt.show()
