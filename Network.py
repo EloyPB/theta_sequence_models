b13ce22b35231890338e7351328594943a5196fc
Eloy <eloyparra.b@gmail.com>
2022-06-06 16:19:00 +0200

    moved tests to class definition files

--- Network.py
+++ Network.py
@@ -236,3 +236,22 @@ class Network(SmartClass):
         ax2.set_xlabel("Time (s)")
         ax2.xaxis.set_ticks_position('bottom')
 
+
+if __name__ == "__main__":
+    config = Config(variants={
+        'LinearTrack': 'OneLap',
+        # 'LinearTrack': 'FixSpeed',
+        'Network': 'Log'
+    })
+    network = Network.current_instance(config)
+
+    # network.track.plot_trajectory()
+    # network.track.plot_features()
+    # network.track.plot_features_heatmap()
+
+    # network.plot_rec_weights()
+    # network.plot_activities(apply_f=0)
+    # network.plot_dynamics(t_end=None)
+    network.plot_activities(apply_f=1, pos_input=1, theta=0, t_start=0)
+
+    plt.show()
--- test_place_fields.py
+++
@@ -1,7 +0,0 @@
-from PlaceFields import PlaceFields, Config, plt
-
-
-pf = PlaceFields.current_instance(Config(pickle_instances=True))
-pf.plot_activations()
-
-plt.show()
