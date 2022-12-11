017eb45663312fe1f9f37769f409da2536656715
Eloy <eloyparra.b@gmail.com>
2022-12-11 16:41:38 +0100

    image showing relationship between fields and sweeps

--- misc/animation_sweeps.py
+++ misc/animation_sweeps.py
@@ -2,24 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 from matplotlib.animation import FuncAnimation
-
-
-# purple - blue - cyan colormap
-c0 = np.array([209, 107, 165])/255
-c1 = np.array([134, 168, 231])/255
-c2 = np.array([95, 251, 241])/255
-color_dict = {'red':   [[0,  c0[0], c0[0]],
-                        [0.50,  c1[0], c1[0]],
-                        [1.0,  c2[0], c2[0]]],
-
-              'green': [[0,  c0[1], c0[1]],
-                        [0.50,  c1[1], c1[1]],
-                        [1.0,  c2[1], c2[1]]],
-
-              'blue':  [[0,  c0[2], c0[2]],
-                        [0.50,  c1[2], c1[2]],
-                        [1.0,  c2[2], c2[2]]]}
-cm = LinearSegmentedColormap('PurpleCyan', color_dict)
+from theta_color_map import cm
 
 
 length = 120
@@ -108,6 +91,8 @@ def update(t_step):
     t = t_step * dt
     ln.set_data(x_log[t_log < t], field[t_log < t])
     sc.set_array(np.where(spike_times < t, spike_phase_log, np.nan))
+    if t_step == 250:
+        fig.savefig("figures/sweep.pdf")
     return [rat1, rat2, ln, sc]
 
 
---
+++ misc/field_and_sweeps.py
@@ -0,0 +1,66 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from theta_color_map import cm
+from small_plots import *
+
+
+length = 100
+theta_distance = 40
+dt = 0.001
+x_start = 0
+x_end = 100
+v = 41.1
+sigma = 3
+num_laps = 50
+
+x = np.linspace(0, length, 1000)
+true_field = np.exp(-(x - length/2)**2/(2*sigma**2)) * 0.85 + 0.1
+measured_field = np.max(np.exp(-(np.linspace(x - theta_distance/2, x + theta_distance/2, 100) - length/2)**2/(2*sigma**2)), axis=0)
+measured_field = measured_field * 0.85 + 0.1
+
+x_log = [x_start]
+r_log = [x_start - theta_distance/2]
+t_log = np.arange(0, num_laps * (x_end - x_start)/v, dt)
+spike_x_log = []
+spike_phase_log = []
+
+for t in t_log[1:]:
+    theta_phase = (t % 0.125) / 0.125 - 0.5  # goes between -0.5 and 0.5
+    x_log.append(x_log[-1] + v * dt)
+    if x_log[-1] >= x_end:
+        x_log[-1] = x_start
+    if 24.5 < x_log[-1] < 25.5 and theta_phase > 0.48:
+        print()
+    r_log.append(x_log[-1] + theta_distance * theta_phase)
+    if np.random.random() < 30 * dt * np.exp(-(r_log[-1] - length/2)**2/(2*sigma**2)):
+        spike_phase_log.append((theta_phase + 0.5) * 360)
+        spike_x_log.append(x_log[-1])
+
+
+print((np.array(r_log) - np.array(x_log)).max())
+
+fig, ax = plt.subplots(2, sharex='col', figsize=(10*CM, 5*CM), constrained_layout=True)
+ax[0].set_xlim((0, length))
+ax[0].set_ylim((0, 1))
+ax[0].axis('off')
+rect = plt.Rectangle((0, 0), length, 0.1, facecolor='lightgray', edgecolor=None, zorder=0)
+ax[0].add_patch(rect)
+ax[0].plot(x, true_field, color='C2')
+ax[0].plot(x, measured_field, color='C0')
+
+sweep = ax[0].imshow(np.vstack((np.linspace(0, 1, 100), np.linspace(0, 1, 100))), cmap=cm,
+                     extent=(length/2 - theta_distance/2, length/2 + theta_distance/2, 0, 0.1), aspect='auto')
+
+
+sc = ax[1].scatter(spike_x_log, spike_phase_log, c=spike_phase_log, vmin=0, vmax=360, cmap=cm)
+ax[1].set_ylim((0, 360))
+ax[1].spines.right.set_visible(False)
+ax[1].spines.top.set_visible(False)
+# ax[1].set_xticks([])
+ax[1].set_yticks([0, 180, 360])
+ax[1].set_xlabel("Position")
+ax[1].set_ylabel(r"$\theta$ phase")
+
+fig.savefig(f"figures/sweep_{theta_distance}.pdf")
+
+plt.show()
